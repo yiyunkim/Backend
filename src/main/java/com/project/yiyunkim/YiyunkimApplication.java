d9c0c37694908c16529f40015610b9c2c4c96095
kim junsoo <kjs201105@naver.com>
2025-09-13 23:04:49 +0900

    initial commit

---
+++ src/main/java/com/project/yiyunkim/YiyunkimApplication.java
@@ -0,0 +1,13 @@
+package com.project.yiyunkim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiyunkimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YiyunkimApplication.class, args);
+	}
+
+}
